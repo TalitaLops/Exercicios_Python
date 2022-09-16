96c5669707ce212527fa8dbeb42e73a7aedb4356
Talita Lopes <talitalopessilva.2020@gmail.com>
2022-09-15 21:11:39 -0300

    Add files via upload

---
+++ Ex 01.py
@@ -0,0 +1,6 @@
+idade = 20;
+if idade<20:
+    print('Você é jovem \n');
+
+else:
+        print('Você não é jovem');
---
+++ Ex 02.py
@@ -0,0 +1,6 @@
+y = float(input('Digite um numero: '));
+parte = y/5
+if (parte < 50) or (parte > 1000:
+    print('A quinta parte desse numero é: ', parte)
+else:
+    print ('O numero não está dentro das condições')
